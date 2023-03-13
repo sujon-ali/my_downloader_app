1a88c375bea4944c3b7af71844af26521e8d8639
Sujon Ali <62113916+sujon-ali@users.noreply.github.com>
2023-03-13 19:42:49 +0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,26 +1,10 @@
 const downloadBtn = document.querrySelector('#download-btn');
 
 function downloadVideo() {
+  const msgTag = document.querrySelector('#message-tag');
   const urlTag = document.querrySelector('#url-tag');
-  const videoUrl = `${urlTag.value}`;
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', videoUrl, true);
-  xhr.responseType = 'blob';
-  xhr.onload = function() {
-    var urlCreator = window.URL || window.webkitURL;
-    var videoUrl = urlCreator.createObjectURL(this.response);
-    var tag = document.createElement('a');
-    tag.href = videoUrl;
-    tag.target = '_blank';
-    tag.download = 'sample.mp4';
-    document.body.appendChild(tag);
-    tag.click();
-    document.body.removeChild(tag);
-  };
-  xhr.onerror = err => {
-    alert('Failed to download video!');
-  };
-  xhr.send();
+  const url = `${urlTag.value}`;
+  msgTag.innerText = url;
 }
 
 downloadBtn.addEventListener('click', downloadVideo);
